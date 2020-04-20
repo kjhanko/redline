0abc1063edc17f07c8cd219de20755b5c1bc9e8e
kjhanko <54955882+kjhanko@users.noreply.github.com>
2020-04-20 09:28:38 -0400

    Added map, fonts, and test test to index.html. Added style.css file.

--- js/main.js
+++ js/main.js
@@ -1,1 +1,21 @@
 //main js file
+
+//Step 1: Create the Leaflet map
+function createMap() {
+
+  //create the map
+  var map = L.map('map', {
+    center: [40, -95],
+    zoom: 5,
+  });
+
+  //add OSM base tilelayer
+  L.tileLayer('http://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> | &copy; <a href="http://cartodb.com/attributions">CartoDB</a> | <a href="https://www.census.gov">US Census Data</a>'
+  }).addTo(map);
+
+  //call getData function
+  // getData(map);
+}
+
+$(document).ready(createMap);
