68a778392c0bc98d47a2b86415feca746c4f6adb
kjhanko <54955882+kjhanko@users.noreply.github.com>
2020-04-21 13:18:19 -0400

    Added seach capability to map. Uses a plug-in to zoom to searched location.

--- js/main.js
+++ js/main.js
@@ -1,18 +1,33 @@
 //main js file
 
-//Step 1: Create the Leaflet map
+//Create the map
 function createMap() {
 
-  //create the map
+  //creates a Leaflet map centered on CONUS
   var map = L.map('map', {
     center: [40, -95],
     zoom: 5,
   });
 
   //add OSM base tilelayer
-  L.tileLayer('http://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png', {
+  L.tileLayer('http://{s}.basemaps.cartocdn.com/rastertiles/light_all/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> | &copy; <a href="http://cartodb.com/attributions">CartoDB</a>'
   }).addTo(map);
+
+  //using Leaflet's ControlSearch with OSM to allow seaching of addresses
+  map.addControl(new L.Control.Search({
+    url: 'https://nominatim.openstreetmap.org/search?format=json&q={s}',
+    jsonParam: 'json_callback',
+    propertyName: 'display_name',
+    propertyLoc: ['lat', 'lon'],
+    marker: L.circleMarker([0, 0], { radius: 30 }),
+    autoCollapse: true,
+    autoType: false,
+    minLength: 2,
+    position: 'topleft',
+    initial: false,
+    zoom: 16,
+  }));
 
   //call getData function
   // getData(map);
